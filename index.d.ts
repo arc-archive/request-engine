fcc3631bfecfc47a35a048bdbfbd60bda4c9b7a6
Pawel Psztyc <jarrodek@gmail.com>
2020-11-28 21:14:23 -0800

    fix: adding registry export
    Signed-off-by: Pawel Psztyc <jarrodek@gmail.com>

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,5 @@
 export { RequestFactory } from './src/RequestFactory';
+export { ModulesRegistry } from './src/ModulesRegistry';
 export { ArcFetchRequest } from './src/ArcFetchRequest';
 export { default as RequestAuthorization } from './src/modules/RequestAuthorization';
 export { default as ResponseAuthorization } from './src/modules/ResponseAuthorization';
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 export { RequestFactory } from './src/RequestFactory.js';
+export { ModulesRegistry } from './src/ModulesRegistry.js';
 export { ArcFetchRequest } from './src/ArcFetchRequest.js';
 export { default as RequestAuthorization } from './src/modules/RequestAuthorization.js';
 export { default as ResponseAuthorization } from './src/modules/ResponseAuthorization.js';
